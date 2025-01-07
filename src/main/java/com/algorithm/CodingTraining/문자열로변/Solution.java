f0a3ab70c846e8e91eec36438fa3fd2a19eb391c
Jinseonghyun <jinsunghyun9@gmail.com>
2025-01-08 01:55:45 +0900

    문자열로 변환

---
+++ src/main/java/com/algorithm/CodingTraining/문자열로변/Solution.java
@@ -0,0 +1,10 @@
+package com.algorithm.CodingTraining.문자열로변;
+
+class Solution {
+    public String solution(int n) {
+        String answer = "" + n;
+        return answer;
+
+        // 문자열 + 정수는 문자열이다.
+    }
+}
---
+++ src/main/java/com/algorithm/CodingTraining/문자열로변/Solution1.java
@@ -0,0 +1,7 @@
+package com.algorithm.CodingTraining.문자열로변;
+
+class Solution1 {
+    public String solution(int n) {
+        return String.valueOf(n);
+    }
+}
