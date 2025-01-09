626a16b449db8e1af18d17463ecbe2b942fd74ba
Jinseonghyun <jinsunghyun9@gmail.com>
2025-01-09 15:29:15 +0900

    문자열의 앞 n글자

---
+++ src/main/java/com/algorithm/CodingTraining/문자열의앞n글자/Solution.java
@@ -0,0 +1,9 @@
+package com.algorithm.CodingTraining.문자열의앞n글자;
+
+class Solution {
+    public String solution(String my_string, int n) {
+        return my_string.substring(0, n);
+    }
+
+    // my_string.substring(0, n) 메서드를 사용하여 my_string의 인덱스 0부터 n까지의 부분 문자열을 추출
+}
