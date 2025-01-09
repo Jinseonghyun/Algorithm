1efb137e7e1e5d0f3b7c8f86acf88a6f0072e4a8
Jinseonghyun <jinsunghyun9@gmail.com>
2025-01-09 16:35:19 +0900

    글자 지우기

---
+++ src/main/java/com/algorithm/CodingTraining/글자지우기/Solution.java
@@ -0,0 +1,18 @@
+package com.algorithm.CodingTraining.글자지우기;
+
+class Solution {
+
+    public String solution(String my_string, int[] indices) {
+        String answer = "";
+        String[] tmp = my_string.split("");
+
+        for (int i = 0; i < indices.length; i++) {
+            tmp[indices[i]] = "";
+        }
+
+        for (String x : tmp) {
+            answer += x;
+        }
+        return answer;
+    }
+}
