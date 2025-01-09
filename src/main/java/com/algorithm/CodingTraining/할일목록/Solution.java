cb42bc89e8b45d3a407468b212d6c9a288466fa2
Jinseonghyun <jinsunghyun9@gmail.com>
2025-01-09 14:42:16 +0900

    할 일 목록

---
+++ src/main/java/com/algorithm/CodingTraining/할일목록/Solution.java
@@ -0,0 +1,12 @@
+package com.algorithm.CodingTraining.할일목록;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        String str = "";
+        for(int i=0; i<finished.length; i++){
+            str = finished[i] == false ? str + todo_list[i] + "," : str;
+        }
+
+        return str.split(",");
+    }
+}
