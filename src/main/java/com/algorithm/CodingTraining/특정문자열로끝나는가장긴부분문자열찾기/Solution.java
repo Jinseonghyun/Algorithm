ffeffb616d124d9466e5728d0cc9f4a9c2b71f6a
Jinseonghyun <jinsunghyun9@gmail.com>
2025-01-09 16:52:13 +0900

    특정 문자열로 끝나는 가장 긴 부분 문자열 찾기

---
+++ src/main/java/com/algorithm/CodingTraining/특정문자열로끝나는가장긴부분문자열찾기/Solution.java
@@ -0,0 +1,13 @@
+package com.algorithm.CodingTraining.특정문자열로끝나는가장긴부분문자열찾기;
+
+class Solution {
+    public String solution(String myString, String pat) {
+        String answer = "";
+
+        int idx = myString.lastIndexOf(pat);
+
+        answer = myString.substring(0, idx) + pat;
+
+        return answer;
+    }
+}
