cb89fde697ee9b940032175c8dc193a1bcae23b6
Jinseonghyun <jinsunghyun9@gmail.com>
2025-01-09 16:15:34 +0900

    공백으로 구분하기 1

---
+++ src/main/java/com/algorithm/CodingTraining/공백으로구분하기1/Solution.java
@@ -0,0 +1,8 @@
+package com.algorithm.CodingTraining.공백으로구분하기1;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = my_string.split(" ");
+        return answer;
+    }
+}
