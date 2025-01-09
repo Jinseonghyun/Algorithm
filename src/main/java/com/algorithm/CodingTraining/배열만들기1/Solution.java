23181fc52a2ec567f3bb6001acfb70c29786196d
Jinseonghyun <jinsunghyun9@gmail.com>
2025-01-09 15:24:49 +0900

    배열 만들기1

---
+++ src/main/java/com/algorithm/CodingTraining/배열만들기1/Solution.java
@@ -0,0 +1,16 @@
+package com.algorithm.CodingTraining.배열만들기1;
+
+class Solution {
+    public int[] solution(int n, int k) {
+        int count = n / k;
+
+
+        int[] answer = new int[count];
+
+        for (int i = 1; i <= count; i++) {
+            answer[i - 1] = k * i;
+        }
+
+        return answer;
+    }
+}
