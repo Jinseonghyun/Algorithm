e18178da6f608a23e6747cf102427522d8086ae7
Jinseonghyun <jinsunghyun9@gmail.com>
2025-01-09 15:55:59 +0900

    수 조작하기

---
+++ src/main/java/com/algorithm/CodingTraining/수조작하기2/Solution.java
@@ -0,0 +1,18 @@
+package com.algorithm.CodingTraining.수조작하기2;
+
+class Solution {
+    public String solution(int[] numLog) {
+        int num = numLog[0];
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 1;i < numLog.length;i++) {
+            if (numLog[i] - num == 1) sb.append("w");
+            else if (numLog[i] - num == -1) sb.append("s");
+            else if (numLog[i] - num == 10) sb.append("d");
+            else sb.append("a");
+            num = numLog[i];
+        }
+
+        return sb.toString();
+    }
+}
