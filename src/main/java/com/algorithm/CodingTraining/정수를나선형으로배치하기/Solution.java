96857cb6c6de4348cc091cbc31f396e4e3088d46
Jinseonghyun <jinsunghyun9@gmail.com>
2025-01-14 01:30:14 +0900

    정수를 나선형으로 배치하기

---
+++ src/main/java/com/algorithm/CodingTraining/정수를나선형으로배치하기/Solution.java
@@ -0,0 +1,31 @@
+package com.algorithm.CodingTraining.정수를나선형으로배치하기;
+
+class Solution {
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+        int num=1;
+        int start=0;
+        int end=n;
+
+        while(num <= n*n){
+
+            //->
+            for(int j=start;j<end;j++)
+                answer[start][j]=num++;
+            //v
+            for(int i=start+1;i<end;i++)
+                answer[i][end-1]=num++;
+            //<
+            for(int j=end-2;j>=start;j--)
+                answer[end-1][j]=num++;
+            //^
+            for(int i=end-2;i>start;i--)
+                answer[i][start]=num++;
+            start++;
+            end--;
+
+        }
+
+        return answer;
+    }
+}
